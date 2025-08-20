74a115c48744330be939a68c4c9d9451c123f222
patel@CM <patelgaurang582001@gmail.com>
2025-08-20 16:58:13 +0530

    Add Functionality of code and optimize

--- binarySearch.py
+++ binarySearch.py
@@ -1,30 +1,59 @@
 def binarySerch(data,Target):
-    for i in range(len(data)-1):
-        for j in range(len(data)-i-1):
-            if data[j] > data[j+1]:
-                data[j],data[j+1] = data[j+1],data[j]
-    print(f"Ascending : {data}")
-    start = 0
-    end = len(data)-1
-    while start < end:
-        mid = int((start+end)/2)
-        if Target > data[mid]:
-            start = mid+1
-        elif Target < data[mid]:
-            end = mid-1
-        elif Target == data[mid]:
-            print(f" Your Target Value {Target} Index is {mid}")
+    #Bubble Sort
+    while True:
+        choice = input("select your choice into Below Option \n (1) Ascending \n (2) Descending\nPlease Enter Your Choice :")
+        if not choice.isdigit():
+            print(" Error : Please Enter Only Digits")
+            continue
+        choice = int(choice)
+        start = 0
+        index = -1
+        end = len(data)-1
+        #Ascending
+        if choice == 1:
+            for i in range(len(data)-1):
+                for j in range(len(data)-i-1):
+                    if data[j] > data[j+1]:
+                        data[j],data[j+1] = data[j+1],data[j]
+            
+            while start <= end:
+                mid = int((start+end)/2)
+                if Target > data[mid]:
+                    start = mid+1
+                elif Target < data[mid]:
+                    end = mid-1
+                else:
+                    index = mid
+                    break
+            break
+        #Descending
+        elif choice == 2:
+            for i in range(len(data)-1):
+                for j in range(len(data)-i-1):
+                    if data[j] < data[j+1]:
+                        data[j],data[j+1] = data[j+1],data[j]
+            while start <= end:
+                mid = int((start+end)/2)
+                if Target > data[mid]:
+                    end = mid-1
+                elif Target < data[mid]:
+                    start = mid+1
+                else:
+                    index = mid
+                    break
+            
             break
         else:
-            print(f" This list does not contain your target. So Return {-1}")
+            print(" Error : Invalid Choice ")
+    if index == -1:
+        print(f"Your Target Not Found -1  ")
+    else:
+        print(f" Your Target Value {Target} Index is {index}")
 
-            
-                 
-print("--------Bubble Sort-------")
-data = [50,20,70,40,30,10,60]
-print(f" Static List : {data}")
+print("--------Binary Search-------")
+data = [50,20,70,40,30,10,60,80]
 while True:
-    Target = input("Enter Your Circular Queue capacity :")
+    Target = input("Enter Your Find Element :")
     if not Target.isdigit():
         print('Please Enter Digit Only')
         continue
