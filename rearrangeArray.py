cb46599fc7a498e64814413b7c250f649c84a2f2
patel@CM <patelgaurang582001@gmail.com>
2025-08-21 16:06:11 +0530

    space remove

--- rearrangeArray.py
+++ rearrangeArray.py
@@ -3,7 +3,6 @@ def rearrangeArray(array):
     for i in range(len(array)):
         index = array[i]
         rearrange_array[index] = i
-
     print(rearrange_array)
 # array = [2,3,8,1,4,0,9,7,6,5]
 array = [4, 0, 3, 1, 2]
