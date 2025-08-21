61d19069dda77f8de4de7e02416b83eb5cc8f8df
patel@CM <patelgaurang582001@gmail.com>
2025-08-21 16:27:28 +0530

    change in lcm gcm program find bugs

--- LCM_GCM.py
+++ LCM_GCM.py
@@ -9,9 +9,7 @@ def lcm(num_1,num_2):
             lcm = greater
             break
         greater+=1
-            
-    
-            
+    return lcm
 def gcm(num_1,num_2):
     if num_1 < num_2:
         smaller = num_1
@@ -20,8 +18,7 @@ def gcm(num_1,num_2):
     for i in range(1,smaller+1):
         if num_1%i == 0 and num_2%i==0:
             gcd = i
-    print(f" {num_1} and {num_2} GCM is {gcd}")
-
+    return gcd
 while True:
     try:
         choice = int(input("select your choice into Below Option \n (1) LCM \n (2) GCM\n (3) Exit\n Please Enter Your Choice :"))
@@ -32,7 +29,7 @@ while True:
         elif choice == 2:
             num_1 = int(input("Enter Number 1 :"))
             num_2 = int(input("Enter Number 2 :"))
-            print(f"{num_1} and {num_2} LCM is {gcm(num_1, num_2)}")
+            print(f"{num_1} and {num_2} GCD is {gcm(num_1, num_2)}")
         elif choice == 3:
             print(f" LCM and GCM Find Stop ")
             break
--- rearrangeArray.py
+++ rearrangeArray.py
@@ -4,6 +4,7 @@ def rearrangeArray(array):
         index = array[i]
         rearrange_array[index] = i
     print(rearrange_array)
+    
 # array = [2,3,8,1,4,0,9,7,6,5]
 array = [4, 0, 3, 1, 2]
 rearrangeArray()
