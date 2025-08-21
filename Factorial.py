19a852e33cccedfdc7fa9dae13309d0594347411
patel@CM <patelgaurang582001@gmail.com>
2025-08-21 12:36:25 +0530

    Change Print Line of Get input number

--- Factorial.py
+++ Factorial.py
@@ -1,12 +1,5 @@
 # Find the Factorial of number optimiz it as much as you can 
 # Function Based 
-
-# #Using Inbuild Math Library using Factorial find
-# def factorial(Number):
-#     import math
-#     return math.factorial(Number)
-
-# Using Loop Find Factorial 
 def factorial(Number):
     fact = 1
     for i in range(1,Number+1):
@@ -14,7 +7,7 @@ def factorial(Number):
     return fact
 
 while True:
-    Number = input("Enter Your Circular Queue capacity :")
+    Number = input("Enter Your Number to find Factorial :")
     if not Number.isdigit():
         print('Please Enter Digit Only')
         continue
