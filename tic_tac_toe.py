75a82241a2ed34f2d1368ccc7fc0f4449844a644
patel@CM <patelgaurang582001@gmail.com>
2025-08-22 11:50:30 +0530

    create tac tic toe game

---
+++ tic_tac_toe.py
@@ -0,0 +1,10 @@
+class TicTacToe():
+    def __int__(self)
+        pass
+
+
+print(" Welcome To Tac Tic Toe Game",
+      " Please Select Belowe Option",
+      "    (1)Play with two player",
+      "    (2)Play Agaist Computer"
+      )
